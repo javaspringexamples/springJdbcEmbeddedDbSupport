f2e3608b2d7ea34512ace2133e8171b432b8c0f2
Codercocoon <mounir.sahrani@gmail.com>
2019-04-10 15:46:14 +0200

    Adding sources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into USER (NAME, USER_NAME, LOCKED) values ('Mounir SAHRANI','javaspringexamples',false);
---
+++ src/test/java/javaspringexamples/springJDBC/EmbeddedDbSupport/EmbeddedDataSourceTest.java
@@ -0,0 +1,42 @@
+package javaspringexamples.springJDBC.EmbeddedDbSupport;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+/**
+ *
+ * @author mounir.sahrani@gmail.com
+ *
+ */
+public class EmbeddedDataSourceTest {
+	private DataSource dataSource;
+
+	@Before
+	public void setUp() {
+		dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:schema.sql")
+				.addScript("classpath:data.sql").build();
+	}
+
+	@Test
+	public void testDataAccessLogic() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		Assert.assertFalse(connection.isClosed());
+		connection.close();
+		Assert.assertTrue(connection.isClosed());
+	}
+
+	@After
+	public void tearDown() {
+		((EmbeddedDatabase) dataSource).shutdown();
+	}
+}
